630444f7924465e2bab092242867dbe93f75197d
chahin-fh <chahinfhima@gmail.com>
2025-08-04 13:56:53 +0100

    fazit tab3a il deploy

--- backend/laboissim/laboissim/settings.py
+++ backend/laboissim/laboissim/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'laboissim.laboissim.urls'
+ROOT_URLCONF = 'laboissim.urls'
 
 TEMPLATES = [
     {
@@ -76,12 +76,14 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'laboissim.wsgi.application'
+WSGI_APPLICATION = 'laboissim.laboissim.wsgi.application'
 
 
 # Database
@@ -154,7 +156,7 @@ REST_FRAMEWORK = {
     ),
 }
 AUTHENTICATION_BACKENDS = [
-    '.laboissim.laboissim.auth_backend.EmailBackend',
+    'laboissim.laboissim.auth_backend.EmailBackend',
     'django.contrib.auth.backends.ModelBackend',
     'social_core.backends.google.GoogleOAuth2',
 ]
@@ -162,12 +164,12 @@ AUTHENTICATION_BACKENDS = [
 SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
     'social_core.pipeline.social_auth.social_uid',
-    'laboissim.social_pipeline.prevent_duplicate_email',  # Custom step to prevent duplicate emails
+    'laboissim.laboissim.social_pipeline.prevent_duplicate_email',  # Custom step to prevent duplicate emails
     'social_core.pipeline.social_auth.auth_allowed',
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',
-    'laboissim.social_pipeline.save_profile',  # Custom step to save extra info
+    'laboissim.laboissim.social_pipeline.save_profile',  # Custom step to save extra info
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
